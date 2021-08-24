2e514de8f4f4b819d7b0263fd494bfb494fbe7c0
KonaDaniel <79358753+KonaDaniel@users.noreply.github.com>
2021-08-24 13:00:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ recognition.onresult = function(event){
 
     console.log(event);
     
-    var Content = event.results[0][0].transscript;
+    var Content = event.results[0][0].transcript;
 
     document.getElementById("text_box").innerHTML = Content;
 
