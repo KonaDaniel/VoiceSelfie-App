a8499d9919ae005f154086755af023ba572c02c1
KonaDaniel <79358753+KonaDaniel@users.noreply.github.com>
2021-08-25 12:31:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -38,6 +38,13 @@ function speak(){
     synth.speak(utterThis);
 
     Webcam.attach(camera);
+
+    setTimeout(function()
+
+     {
+      take_snapshot();
+      save();
+    }, 5000);
 }
 
 camera = document.getElementById("camera");
@@ -49,4 +56,18 @@ Webcam.set({
     jpeg_quality: 90 
 });
 
+function take_snapshot(){
+
+    Webcam.snap(function(data_url){
+        document.getElementById("result").innerHTML = ' <img id="selfie_img" src="" '+ data_uri +' "/>';
+        
+    });
+}
+
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
 
